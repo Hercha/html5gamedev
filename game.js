4a3b27c9c520846940dea55021d469bafb17b5c0
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-17 23:29:20 +0200

    multiple sprite states, removing sprites

--- game.js
+++ game.js
@@ -15,20 +15,6 @@ class Game {
         }
     }
     
-    spawn() {
-        const sprite = new Sprite({
-           context: this.context,
-            x: Math.random() * this.canvas.width,
-            y: Math.random() * this.canvas.height,
-            width: this.spriteImage.width,
-            height: this.spriteImage.height,
-            image: this.spriteImage
-        });
-        
-        this.sprites.push(sprite);
-        this.sinceLastSpawn = 0;
-    }
-    
     refresh() {
         const now = Date.now();
         const dt = (now - this.lastRefreshTime)/1000.0;
@@ -49,9 +35,49 @@ class Game {
         if(this.sinceLastSpawn > 1) {
             this.spawn();
         }
+        let removed;
+
+        do {
+            removed = false;
+            for(let sprite of this.sprites) {
+                if(sprite.kill) {
+                    const index = this.sprites.indexOf(sprite);
+                    this.sprites.splice(index, 1);
+                    removed = true;
+                    break;
+                }
+            }
+        } while(removed);
+
+        for(let sprite of this.sprites) {
+            if(sprite == null) {
+                continue;
+            }
+            sprite.update(dt);
+        }
+    }
+    
+    spawn() {
+        const sprite = new Sprite({
+            context: this.context,
+            x: Math.random() * this.canvas.width,
+            y: Math.random() * this.canvas.height,
+            width: this.spriteImage.width,
+            height: this.spriteImage.height,
+            image: this.spriteImage,
+            states: [ 
+                    { mode:"spawn", duration: 0.5 },
+                    { mode:"static", duration: 1.5 },
+                    { mode:"die", duration: 0.8 }
+            ]
+        });
+        
+        this.sprites.push(sprite);
+        this.sinceLastSpawn = 0;
     }
     
     render() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for(let sprite of this.sprites) {
             sprite.render();
         }
@@ -66,9 +92,80 @@ class Sprite {
         this.image = options.image;
         this.x = options.x;
         this.y = options.y;
+        this.states = options.states;
+        this.state = 0;
+        this.scale = (options.scale == null) ? 1.0 : options.scale;
+        this.opacity = (options.opacity == null) ? 1.0 : options.opacity;
+        this.currentTime = 0;
+        this.kill = false;
+    }
+    
+    set state(index) {
+        this.stateIndex = index;
+        this.stateTime = 0;
+    }
+    
+    get state() {
+        let result;
+        if(this.stateIndex < this.states.length) {
+            result = this.states[this.stateIndex];
+            
+            return result;
+        }
+    }
+    
+    update(dt) {
+        this.stateTime += dt;
+        const state = this.state;
+        
+        if(state == null) {
+            this.kill = true;
+            return;
+        }
+        
+        const delta = this.stateTime/state.duration;
+        
+        if(delta > 1 ) {
+            this.state = this.stateIndex + 1;
+        }
+        
+        switch(state.mode) {
+            case "spawn":
+                // scale and fade in
+                this.scale = delta;
+                this.opacity = delta;
+                break;
+            case "static":
+                this.scale = 1.0;
+                this.opacity = 1.0;
+                break;
+            case "die":
+                this.scale = 1.0 + delta;
+                this.opacity = 1.0 - delta;
+                if(this.opacity < 0) {
+                    this.opacity = 0;
+                }
+                break;
+        }
     }
     
     render() {
-        this.context.drawImage(this.image, this.x, this.y);
+        // Draw the animation
+        const alpha = this.context.globalAlpha;
+        
+        this.context.globalAlpha = this.opacity;
+        
+        this.context.drawImage(
+            this.image, 
+            0, 
+            0,
+            this.width,
+            this.height,
+            this.x,
+            this.y,
+            this.width * this.scale,
+            this.height * this.scale);
+        
+        this.context.globalAlpha = alpha;
     }
 }
