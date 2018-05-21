e2470a3f04f719d8dc85fc812202c43edb3bb46b
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-21 02:28:52 +0200

    section 7 continue, check files differents

--- html5gamedev7/game.js
+++ html5gamedev7/game.js
@@ -35,11 +35,13 @@ class Game {
         // Create an engine
         const Bodies = Matter.Bodies;
         const Events = Matter.Events;
+        const Constraint = Matter.Constraint;
         
         this.engine = Matter.Engine.create();
         
         let bodies = [];
         let sprites = [];
+        let pins = [];
         let body;
         const frame = this.spriteData.frames[0].frame;
         const scale = 0.5;
@@ -47,7 +49,17 @@ class Game {
         this.anims = [];
         this.anims.push(new Anim("ambient", {frameData: this.spriteData.frames, frames: [0], fps: fps}));
         this.anims.push(new Anim("walk", {frameData: this.spriteData.frames, frames: [1,"..",7], fps: fps, force: {x: 150, y: 0}}));
-        body = Bodies.rectangle(50, 100, frame.w * scale, frame.h * scale);
+        this.anims.push(new Anim("fall", {frameData: this.spriteData.frames, frames: [16, 17], loop: false, fps: fps}));
+        this.anims.push(new Anim("crash", {frameData: this.spriteData.frames, frames: [18,"..", 30], loop: false, oncomplete: function() {
+            game.bucket.anim = "ambient";
+            Matter.Body.setAngle(game.bucket.physicsBody, 0);
+        } ,fps: fps}));
+        this.anims.push(new Anim("pour", {frameData: this.spriteData.frames, frames: [0,0,0,0,0,0,35,"..",40], loop: false, fps: fps}));
+        
+        const defaultCategory = "0x0001";
+        const draggableCategory = "0x0002";
+        
+        body = Bodies.rectangle(50, 100, frame.w * scale, frame.h * scale, {label: "bucket", collisionFilter: {category: draggableCategory}, friction: 0.0001});
         const options = {
             context: this.context,
             image: this.spriteImage,
@@ -63,14 +75,23 @@ class Game {
         
         this.sprites = sprites;
         
-        bodies.push(Bodies.rectangle(400, 610, 810, 60, {isStatic: true})); // Floor
-        bodies.push(Bodies.rectangle(50, 150, 100, 10, {isStatic: true, label:"clickable"})); // Bucket plattform
-        bodies.push(Bodies.rectangle(-30, 300, 60, 600, {isStatic: true})); // Left side
-        bodies.push(Bodies.rectangle(400, 300, 400, 10, {isStatic: true, angle: 0.4, label:"clickable"})); // Sloping plattform
-        bodies.push(Bodies.rectangle(770, 300, 60, 600, {isStatic: true})); // Right side
+        bodies.push(Bodies.rectangle(400, 610, 810, 60, {isStatic: true, label: "floor", collisonFilter: {defaultCategory} })); // Floor
+        bodies.push(Bodies.rectangle(120, 150, 240, 10, {collisionFilter: {category: defaultCategory} }));
+        bodies.push(Bodies.rectangle(20, 175, 40, 40, {isStatic: true, label:"clickable", collisionFilter: {category: defaultCategory} })); // supports right end of bucket plattform
+        bodies.push(Bodies.rectangle(170, 175, 40, 40, {isStatic: true, label: "clickable", collisionFilter: {defaultCategory} })); // supports right end of bucket platform
+        bodies.push(Bodies.rectangle(-30, 300, 60, 600, {isStatic: true, collisionFilter: {category: defaultCategory} })); // left side
+        bodies.push(Bodies.rectangle(400, 200, 400, 10, {collisionFilter: {category: defaultCategory} }));
+        bodies.push(Bodies.rectangle(260, 220, 40, 40, {isStatic: true, label:"clickable", collisionFilter: {category: defaultCategory} })); // supports left end of middle platform
+        bodies.push(Bodies.rectangle(560, 220, 40, 40, {isStatic: true, label:"clickable", collisionFilter: {category: defaultCategory} })); // supports right end of middle platform
+        bodies.push(Bodies.rectangle(600, 280, 10, 10, {isStatic: true, collisionFilter: {category: defaultCategory}})); // supports right end of middle platform
+        bodies.push(Bodies.rectangle(680, 300, 220, 30, {isStatic: true, label: "exit platform" ,collisionFilter: {category: defaultCategory}})); // Bath platform
+        bodies.push(Bodies.rectangle(760, 270, 80, 30, {isStatic: true,label: "bath" ,collisionFilter: {category: defaultCategory}})); // Bath
+        bodies.push(Bodies.rectangle(795, 300, 10, 600, {isStatic: true, label: "right" ,collisionFilter: {category: defaultCategory}})); // Right side
         
         // add all of the bodies to the world
         Matter.World.add(this.engine.world, bodies);
+        // add all of the pins to the world
+        Matter.World.add(this.engine.world, pins);
         
         // Allow dragging of non-static bodies
         const mouseConstraint = Matter.MouseConstraint.create(this.engine, { // Create Constraint 
@@ -82,6 +103,7 @@ class Game {
                 stiffness: 0.8
             }
         });
+        mouseConstraint.collisionFilter.mask = draggableCategory;
         Matter.World.add(this.engine.world, mouseConstraint);
         
         const game = this;
@@ -96,16 +118,34 @@ class Game {
             this.canvas.addEventListener("mousedown", down);
         }
         
+        Events.on(mouseConstraint, 'startdrag', function(event) {
+            console.log(event);
+            if(event.body.label != "bucket") {
+                Events.trigger(mouseConstraint, 'enddrag');
+            }
+        });
+        
         Events.on(this.engine, 'collisionStart', function(event) {
            const pairs = event.pairs;
             
-            for (let i = 0, j = pairs.length; i != j; ++i) {
-                const pair = pairs[i];
+            for (let pair of pairs) {
+                let bucket;
+                let platform;
                 
                 if(pair.bodyA === game.bucket.physicsBody) {
-                    
+                    bucket = pair.bodyA;
+                    platform = pair.bodyB;
                 } else if(pair.bodyB === game.bucket.physicsBody) {
-                    
+                    bucket = pair.bodyB;
+                    paltform = pair.bodyA;
+                }
+                if(bucket != undefined) {
+                    game.bucket.supported = true;
+                    if(platform.label == "floor") {
+                        game.bucket.anim = "crash";
+                    } else if(platform.label == "bath") {
+                        game.bucket.anim = "pour";
+                    }
                 }
             }
         });
@@ -113,16 +153,23 @@ class Game {
         Events.on(this.engine, 'collisionEnd', function(event) {
             const pairs = event.pairs;
             
-            for(let i = 0, j = pairs.length; i != j; ++i) {
-                const pair = pairs[i];
+            for (let pair of pairs) {
+                let bucket;
+                let platform;
                 
                 if(pair.bodyA === game.bucket.physicsBody) {
-                    
+                    bucket = pair.bodyA;
+                    platform = pair.bodyB;
                 } else if(pair.bodyB === game.bucket.physicsBody) {
-                    
+                    bucket = pair.bodyB;
+                    paltform = pair.bodyA;
+                }
+                if(bucket != undefined) {
+                    //console.log("Bucket collides with " + platform.label);
+                    game.bucket.supported = false;
                 }
             }
-        })
+        });
         
         Events.on(this.engine, 'collisionActive', function(event) {
             const pairs = event.pairs;
@@ -136,7 +183,7 @@ class Game {
 
                 }
             }
-        })
+        });
         
         // run the engine
         Matter.Engine.run(this.engine);
@@ -158,10 +205,27 @@ class Game {
     }
     
     update(dt) {
+        const velocity = this.bucket.physicsBody.velocity;
+        // console.log(`Bucket update ${velocity.x.toFixed(1)}, ${velocity.y.toFixed(1)}`);
+        
+        const animName = this.bucket.animName;
+        
+        if(animName != "crash" && animName != "pour") {
+            if(velocity.x < 1) {
+                this.bucket.anim = "ambient";
+            } else if(velocity.x > 1) {
+                this.bucket.anim = "walk";
+            }
+            
+            if(!this.bucket.supported && velocity.y > 1) {
+                this.bucket.anim = "fall";
+            }
+        }
+        
         this.bucket.update(dt);
     }
     
-    debugPhysics(bodies) {
+    debugPhysics(bodies, constraints) {
         this.context.beginPath();
         
         for(let body of bodies) {
@@ -179,10 +243,28 @@ class Game {
         this.context.lineWidth = 1;
         this.context.strokeStyle ='#999';
         this.context.stroke();
+        
+        this.context.beginPath();
+        
+		for (let constraint of constraints) {
+			switch (constraint.render.type){
+				case "pin":
+					this.context.moveTo(constraint.pointB.x, constraint.pointB.y);
+					this.context.arc(constraint.pointB.x, constraint.pointB.y, 2, 0, 2*Math.PI);
+					let pos = Matter.Vector.add(constraint.bodyA.position, constraint.pointA);
+					this.context.arc(pos.x, pos.y, 2, 0, 2*Math.PI);
+					break;
+			}
+		}
+        
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = '#933';
+        this.context.stroke();
     }
     
     render () {
         let bodies = Matter.Composite.allBodies(this.engine.world);
+        let constraints = Matter.Composite.allConstraints(this.engine.world);
         
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         
@@ -191,7 +273,7 @@ class Game {
         }
         
         if(this.debug) {
-            this.debugPhysics(bodies);
+            this.debugPhysics(bodies, constraints);
         }
     }
     
