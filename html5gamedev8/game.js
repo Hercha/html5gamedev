09f55a5e67b4ad7e0b71861d9b18ec35f6d61078
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-22 22:56:03 +0200

    setting up class Game for bear game

---
+++ html5gamedev8/game.js
@@ -0,0 +1,49 @@
+class Game {
+    constructor() {
+        
+    }
+    
+    loadJSON(json, callback) {
+        
+    }
+    
+    init() {
+        
+    }
+    
+    jumpComplete() {
+        
+    }
+    
+    resetBear() {
+        
+    }
+    
+    nextLevel() {
+        
+    }
+    
+    refresh() {
+        
+    }
+    
+    update(dt) {
+        
+    }
+    
+    spawn() {
+        
+    }
+    
+    render() {
+        
+    }
+    
+    getMousePos(evt) {
+        
+    }
+    
+    tap(evt) {
+        
+    }
+}
