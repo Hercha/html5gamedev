9fc91d4fdaeaabc4697533e0d254aab1804cb10c
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-22 23:21:01 +0200

    setting up constructor

--- html5gamedev8/game.js
+++ html5gamedev8/game.js
@@ -1,6 +1,56 @@
 class Game {
     constructor() {
+        this.canvas = document.getElementById("game");
+        this.context = this.canvas.getContext("2d");
+        this.lastRefreshTime = Date.now();
+        this.sinceLastSpawn = 0;
+        this.sprites = [];
+        this.score = 0;
+        this.spriteData;
+        this.spriteImage;
+        this.icebergs = [];
+        this.bear;
+        this.platforms = [];
+        this.buttons = [];
+        this.ui = [];
+        this.sprites = [];
         
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext());
+        this.collectSfx = new SFX({
+            context: this.audioContext,
+            src: {mp3: "sfx/collect.mp3", webm: "sfx/collect.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        this.blowholeSfx = new SFX({
+            context: this.audioContext,
+            src: {mp3: "sfx/blowhole.mp3", webm: "sfx/collect.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        this.dipSfx = new SFX({
+            context: this.audioContext,
+            src: {mp3: "sfx/splash_dip.mp3", webm: "sfx/splas_dip.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        this.diveSfx = new SFX({
+            context: this.audioContext,
+            src: {mp3: "sfx/splash_dive.mp3", webm: "sfx/splash_dive.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        
+        const game = this;
+        
+        this.loadJSON("beargame", function(data, game) {
+            game.spriteData = JSON.parse(data);
+            game.spriteImage = new Image();
+            game.spriteImage.src = game.spriteData.meta.image;
+            game.spriteImage.onload = function(){
+                game.init();
+            }
+        });
     }
     
     loadJSON(json, callback) {
