59d323b7c17925129848e3c33de24f8c58e22b80
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-23 13:48:47 +0200

    setting up class

--- html5gamedev9/game.js
+++ html5gamedev9/game.js
@@ -1,3 +1,83 @@
 class Game {
+    constructor(index) {
+        
+    }
+    
+    load() {
+        
+    }
+    
+    assetsLoaded() {
+        
+    }
+    
+    // Loads the sprite data json file
+    loadJSON(fileName, callback) {
+        
+    }
+    
+    // Called after the spritesheet is loaded to initialize the actors and their initial action
+    init() {
+        
+    }
+    
+    getMousePos(evt) {
+        
+    }
+    
+    mousedown(evt) {
+        
+    }
+    
+    mousemove(evt) {
+        
+    }
+    
+    mouseup(evt) {
+        
+    }
+    
+    animNamed(name) {
+        
+    }
+    
+    setAction(spriteName, animName, flipped) {
+        
+    }
+    
+    checkFloor() {
+        
+    }
+    
+    // Triggers an update and render
+    refresh() {
+        
+    }
+    
+    drawPlatforms() {
+        
+    }
+    
+    drawDiamonds() {
+        
+    }
+    
+    checkDiamonds() {
+        
+    }
+    
+    constrainBackground() {
+        
+    }
+    
+    // Update the actors
+    update(dt) {
+        
+    }
+    
+    // Renders all actors
+    render() {
+        
+    }
     
 }
