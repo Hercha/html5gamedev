b5cfddb497c6913574d1d7d3a2531653d9646b90
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-23 14:05:49 +0200

    setting up constructor

--- html5gamedev9/game.js
+++ html5gamedev9/game.js
@@ -1,6 +1,28 @@
 class Game {
     constructor(index) {
+        this.canvas = document.getElementById('game');
+        this.context = this.canvas.getContext("2d");
+        const game = this;
+        const options = {
+            assets: [
+                "background.jpg",
+                "bits.json",
+                "bits.png",
+                "xbloke.json",
+                "xbloke.png"
+            ],
+            oncomplete: function() {
+                const progress = document.getElementById('progress');
+                progress.style.display = "none";
+                game.load();
+            },
+            onprogress: function(value) {
+                const bar = document.getElementById('progress-bar');
+                bar.style.width = `${value * 100}%`;
+            }
+        }
         
+        const preloader = new Preloader(options);
     }
     
     load() {
