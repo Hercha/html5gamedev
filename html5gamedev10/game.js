af7646649f67e241ba0d7fa89b2dd89a4973836d
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-24 09:02:43 +0200

    setting up constructor

--- html5gamedev10/game.js
+++ html5gamedev10/game.js
@@ -1,5 +1,42 @@
 class Game {
     constructor() {
+        this.canvas = document.getElementById("game");
+        this.context = this.canvas.getContext("2d");
+        this.lastRefreshTime = Date.now();
+        this.config = {tilesize: 128};
+        this.computer = {row: -1, col: -1};
+        this.sprites = [];
+        this.ui = [];
+        
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        
+        this.clickSfx = new SFX({
+            context: this.audioContext,
+            crc: {mp3: "sfx/click.mp3", webm: "sfx/click.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        
+        this.errorSfx = new SFX({
+            context: this.audioContext,
+            src: {mp3: "sfx/boing.mp3", webm: "sfx/boing.webm"},
+            loop: false,
+            volume: 0.3
+        });
+        
+        this.state = "loading";
+        this.resize();
+        
+        const game = this;
+        this.loadJSON("reverse1", function(data, game) {
+            game.spriteData = JSON.parse(data);
+            game.spriteImage = new Image();
+            game.spriteImage.src = game.spriteData.meta.image;
+            game.spriteImage.onload = function() {
+                game.init();
+            }
+            
+        });
         
     }
     
