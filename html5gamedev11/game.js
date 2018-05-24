8c8f8b5224bf180150beda0eab8b01bd2d4e931c
Hercha <jens.hedberg@medieinstitutet.se>
2018-05-24 23:08:36 +0200

    setting up section

---
+++ html5gamedev11/game.js
@@ -0,0 +1,98 @@
+class Game {
+    constructor() {
+        
+    }
+
+    loadJSON(json, callback) {
+        
+    }
+    
+    resize() {
+        
+    }
+    
+    get scale() {
+        
+    }
+    
+    shuffleDeck(suits) {
+        
+    }
+    
+    init() {
+        
+    }
+    
+    initGame(button) {
+        
+    }
+    
+    buildSpriteList() {
+        
+    }
+    
+    dealFromStack() {
+        
+    }
+    
+    checkForCompletedPile(pile) {
+        
+    }
+    
+    cardName(index) {
+        
+    }
+    
+    dumpCards(cards, prefix="", startIndex = 0, limit = -1) {
+        
+    }
+    
+    legalMove(pile, index) {
+        
+    }
+    
+    getNextCardCoordinates(col) {
+        
+    }
+    
+    emptyTableauPiles() {
+        
+    }
+    
+    legalDrop(card, dragInfo) {
+        
+    }
+    
+    insertArrayAt(array, index, arrayToInsert) {
+        
+    }
+    
+    refresh() {
+        
+    }
+    
+    update(dt) {
+        
+    }
+    
+    render() {
+        
+    }
+    
+    getMousePos(evt) {
+        
+    }
+    
+    tap(evt) {
+        
+    }
+    
+    move(evt) {
+        
+    }
+    
+    up(evt) {
+        
+    }
+    
+}
